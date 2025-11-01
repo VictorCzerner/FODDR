fc546ecc8db30b385b59500a174cdc73e4b14143
VictorCzerner <victor.czerner@edu.pucrs.br>
2025-11-01 17:55:56 -0300

    maven

---
+++ src/main/java/com/czerner/foddr/FoddrApplication.java
@@ -0,0 +1,13 @@
+package com.czerner.foddr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoddrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoddrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/czerner/foddr/FoddrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czerner.foddr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoddrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
